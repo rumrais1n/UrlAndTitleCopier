02943abf9c7bf6d0fa04707385de9bd0e2f34829
rumrais1n <rumraisin@rumraisin.net>
2018-10-26 23:09:36 +0900

    minor update

--- makeZipPackage.sh
+++ makeZipPackage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-$version=$1
+version=$1
 
 zip -r UrlAndTitleCopier-$version.zip * -x dist/* -x UrlAndTitleCopier.iml -x updateDist.sh -x .* -x ./*/.DS_Store
+mv UrlAndTitleCopier-$version.zip dist/
